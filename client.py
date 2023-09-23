a646f7673964b9a004b2ccfa2894b426050d6f69
Christian <christianfonseca991@gmail.com>
2023-09-22 21:03:10 -0300

    aaa

--- client.py
+++ client.py
@@ -1,14 +1,26 @@
 import websockets
 import asyncio
+from cryptography.fernet import Fernet
+
+# Chave de criptografia (a mesma chave usada no servidor)
+key = b'your-secret-key-here'
+cipher_suite = Fernet(key)
 
 async def mensagem():
- async with websockets.connect("ws://ip da pessoa:8080") as socket:
-   while True:
-    msn = input("Digite uma mensagem: ")
-    if msn == 'sair':
-     break
-    await socket.send(msn)
-    response = await socket.recv()
-    print("Resposta de Mr.Robot: ", response)
-  
+    async with websockets.connect("ws://ip-da-pessoa:8080") as socket:
+        while True:
+            msn = input("Digite uma mensagem: ")
+            if msn == 'sair':
+                break
+
+            # Criptografa a mensagem antes de enviar
+            encrypted_message = cipher_suite.encrypt(msn.encode())
+            await socket.send(encrypted_message)
+
+            response = await socket.recv()
+            
+            # Descriptografa a resposta recebida do servidor
+            decrypted_response = cipher_suite.decrypt(response).decode()
+            print("Resposta de Mr.Robot (descriptografada):", decrypted_response)
+
 asyncio.get_event_loop().run_until_complete(mensagem())
--- serv.py
+++ serv.py
@@ -1,12 +1,30 @@
 import websockets
 import asyncio
+from cryptography.fernet import Fernet
 
-#Função chamada quando um cliente se conectar
+# Chave para criptografar e descriptografar a mensagem
+key = Fernet.generate_key()
+cipher_suite = Fernet(key)
 
+# Função para criptografar uma mensagem
+def encrypt_message(message):
+    encrypted_message = cipher_suite.encrypt(message.encode())
+    return encrypted_message
+
+# Função para descriptografar uma mensagem
+def decrypt_message(encrypted_message):
+    decrypted_message = cipher_suite.decrypt(encrypted_message).decode()
+    return decrypted_message
+
+# Função chamada quando um cliente se conectar
 async def response(websocket, path):
     msn = await websocket.recv()
-    print(f"Mensagem recebida: {msn}")
-    await websocket.send("Mensagem recebida, xero")
+    decrypted_message = decrypt_message(msn)
+    print(f"Mensagem recebida (descriptografada): {decrypted_message}")
+    
+    response_message = "Mensagem recebida, xero"
+    encrypted_response = encrypt_message(response_message)
+    await websocket.send(encrypted_response)
 
 start_server = websockets.serve(response, '0.0.0.0', 8080)
 asyncio.get_event_loop().run_until_complete(start_server)
